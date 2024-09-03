07ed7ec1aac6ccb39869f709a8cc8ac93ffe39f4
MarinaFernandezFromMarseille <168725135+MarinaFernandezFromMarseille@users.noreply.github.com>
2024-09-03 10:49:36 +0200

    setters product class

--- job01/index.php
+++ job01/index.php
@@ -1,6 +1,7 @@
 <?php
 
-class Product {
+class Product
+{
     private $id;
     private $name;
     private $photos = [];
@@ -10,7 +11,8 @@ class Product {
     private $createdAt;
     private $updatedAt;
 
-    public function __construct($id, $name, $photos, $description, $quantity, DateTime $createdAt, DateTime $updatedAt) {
+    public function __construct($id, $name, $photos, $description, $quantity, DateTime $createdAt, DateTime $updatedAt)
+    {
         $this->id = $id;
         $this->name = $name;
         $this->photos = $photos;
@@ -23,46 +25,77 @@ class Product {
     //getters and setters
     //id getter
 
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
-       
-
     }
 
-    public function getName() {
+    public function getName()
+    {
         return $this->name;
     }
-
-    public function getPhotos() {
-        return $this->photos;
+    public function setName($name)
+    {
+        $this->name = $name;
     }
 
-    public function getDescription() {
+    public function getPhotos()
+    {
+        return $this->photos;
+    }
+    public function setPhotos($photos)
+    {
+        $this->photos = $photos;
+    }
+
+    public function getDescription()
+    {
         return $this->description;
     }
 
-    public function getQuantity() {
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function getQuantity()
+    {
         return $this->quantity;
     }
-    public function getCreatedAt() {
+
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+    }
+    public function getCreatedAt()
+    {
         return $this->createdAt;
     }
 
-    public function getUpdatedAt() {
+    public function getUpdatedAt()
+    {
         return $this->updatedAt;
     }
-        
+
+    public function setUptatedAt(DateTime $uptatedAt = null)
+    {
+
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
