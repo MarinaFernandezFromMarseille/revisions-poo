3ba69afeaa68e01029915080b0c7bb87f942f22b
MarinaFernandezFromMarseille <168725135+MarinaFernandezFromMarseille@users.noreply.github.com>
2024-09-04 15:37:00 +0200

    finished job05

--- job05/index.php
+++ job05/index.php
@@ -105,3 +105,47 @@ class products
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+try {
+    // Connexion à la base de données
+    $host = 'localhost'; // L'adresse du serveur de la base de données
+    $dbname = 'draft_shop'; // Le nom de la base de données
+    $username = 'root'; // Le nom d'utilisateur de la base de données
+    $password = ''; // Le mot de passe de la base de données
+
+    // Création de la connexion PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+
+    // Configurer PDO pour afficher les erreurs en mode Exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "SELECT category_id FROM products WHERE id = 7";
+    $stmt = $pdo->query($sql);
+
+    // Récupération des résultats
+    $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $category_id = $products[0]['category_id'];
+
+    echo ($category_id);
+
+
+
+}catch (Exception $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+
+    ?>
+
+    
+</body>
+</html>
